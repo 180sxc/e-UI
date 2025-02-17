3c383ee23f5f3cfcc7592317344d0b2f353c226b
180sxc <xcsplgk@gmail.com>
2025-02-17 23:21:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function qs (item) {
+  return document.querySelector(item)
+}
+window.addEventListener("DOMContentLoaded", () => {
+  var startup = qs(".start-up")
+  function doStartUp() {
+  }
+});
